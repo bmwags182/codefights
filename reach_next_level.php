539fc390fad03153b23e61a211fab1cd7b60d2c9
bmwags182 <bretwagner@bwagner-webdev.com>
2017-01-21 23:51:27 -0600

    Challenge Completed
    given params find out if I will reach the next level when killing the
    next enemy
    
    add functionality later that finds out how many more enemies I would
    need to kill in order to reach the next level

---
+++ reach_next_level.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Built for codefights
+ * Challenge: when gaming, given parameters, find out if killing the next enemy
+ * will advance me to the next level
+ *
+ * @param int $experience amount of curent XP in game
+ * @param int $threshold XP needed to advance to the next level
+ * @param int $reward XP gained for killing next enemy
+ *
+ * @return boolean will I advance to the next level
+ */
+
+function reachNextLevel($experience, $threshold, $reward) {
+    if($experience + $reward >= $threshold) {
+        return true;
+    } else {
+        return false;
+    }
+}
